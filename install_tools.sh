ef566775512d0abb8aa60a4622f745b65013cf94
wverbeke <willem.verbeke@cern.ch>
2022-09-17 22:10:48 +0200

    Add line to install vim-plug

--- install_tools.sh
+++ install_tools.sh
@@ -1,1 +1,5 @@
+# Install ripgrep for searching code.
 sudo apt install ripgrep
+
+# Install vim-plug for easily installing vim extensions.
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
