c825e0d37e623eabeeb745d58873502950e5b1ce
Willem Verbeke <willem_verbeke@hotmail.com>
2022-10-21 22:53:39 +0200

    Add dependencies of YouCompleteMe

--- install_tools.sh
+++ install_tools.sh
@@ -3,3 +3,7 @@ sudo apt install ripgrep
 
 # Install vim-plug for easily installing vim extensions.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install tools necessary for YouCompleteMe vim extension to work.
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
