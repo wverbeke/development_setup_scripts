bdf816d1a33341c7aabd82555392650deba25f84
Willem Verbeke <willem_verbeke@hotmail.com>
2022-11-15 20:15:07 +0100

    add curl install and save default terminal color

--- install_tools.sh
+++ install_tools.sh
@@ -2,6 +2,7 @@
 sudo apt install ripgrep
 
 # Install vim-plug for easily installing vim extensions.
+sudo apt install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install tools necessary for YouCompleteMe vim extension to work.
